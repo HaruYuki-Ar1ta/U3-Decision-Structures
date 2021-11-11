6110c6dce71ed83b9199a0545803550457c21c2f
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-11-11 13:04:37 -0500

    L1 AddMultiply Complete

--- L1-If Statements/src/AddORMultiply.java
+++ L1-If Statements/src/AddORMultiply.java
@@ -1,3 +1,6 @@
+import javax.swing.JOptionPane;
+
+
 public class AddORMultiply {
 
     /*
@@ -7,4 +10,23 @@ public class AddORMultiply {
     If the first one is larger than the second, mutiply and display the result
     If not, add them and display the results.
      */
+
+    public static void main (String[] args){
+    int x = Integer.parseInt(JOptionPane.showInputDialog("What is your first number?"));
+    int y = Integer.parseInt(JOptionPane.showInputDialog("What is the second number?"));
+        if (x > y){
+            int product = multiply(x, y);
+            JOptionPane.showMessageDialog(null, "The product of your numbers are: " + product);
+        }
+        if (x < y){
+            int sum = addition(x, y);
+            JOptionPane.showMessageDialog(null, "The sum of your numbers is: " + sum);
+        }
+    }
+    public static int addition(int x, int y){
+        return x + y;
+    }
+    public static int multiply(int x, int y){
+        return x * y;
+    }
 }
