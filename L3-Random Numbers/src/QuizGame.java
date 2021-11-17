1a2b518465a5801a8a3f65f6dd3d247bbf8bda5f
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-11-17 13:19:32 -0500

    QuizGame not complete

--- L3-Random Numbers/src/QuizGame.java
+++ L3-Random Numbers/src/QuizGame.java
@@ -1,2 +1,18 @@
 public class QuizGame {
+
+    int usersAnswer, correctAnswer;
+
+    public static void main(String[] args) {
+        String operation = "";
+
+
+
+    }
+    public static void addition(){
+
+    }
+
+
+
+
 }
