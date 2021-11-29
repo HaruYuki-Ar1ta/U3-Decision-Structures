660e260cd1d2a3af044729725b0b675a0558ae95
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-11-29 13:19:23 -0500

    Complete

---
+++ L4-Logical Operators/src/ScubaGear.java
@@ -0,0 +1,23 @@
+public class ScubaGear {
+
+    public static void main() {
+
+        int height = 70;
+        int weight = 200;
+
+        if((height >= 62 && height <= 78) && (weight <= 220)){
+            System.out.println("You can rent scuba gear");
+
+        }else{
+            System.out.println("You cannot rent scuba gear");
+        }
+    }
+
+
+
+
+
+
+
+
+}
